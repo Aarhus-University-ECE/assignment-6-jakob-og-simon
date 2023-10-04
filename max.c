6c41fdff5feeb4c87f458693635dd5295589fb94
simonnyman <202305077@post.au.dk>
2023-10-04 13:06:19 +0200

    almost done

--- max.c
+++ max.c
@@ -3,8 +3,22 @@
 #include <stdio.h>
 
 int max(int *numbers, int size) {
-  // Excercise 2
-  // Implement your code below...
+
+/*pre: Pointer numbers points to an array with "size" number of elements*/
+/*post: Returns the largest numbers in the given array*/
+
+  assert(size>0) ; /*Ensures that the array is not empty*/
+    int max_value = numbers[0]; /*initially assume that the first element is the largest*/
+
+    for (int i = 1; i < size; i++) {
+        if (numbers[i] > max_value) {
+            max_value = numbers[i]; /*Update max_value if a larger number is found*/
+        }
+    }
+
+    return max_value;
+
+
 
   return -1;
 }
