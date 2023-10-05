4185f706a666aec23bfc4e446c8474da9e0c0116
simonnyman <202305077@post.au.dk>
2023-10-05 13:48:47 +0200

    Done

--- list.c
+++ list.c
@@ -1,7 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "list.h"
 
 void add(node *head, int x) {
@@ -21,8 +20,19 @@ void add(node *head, int x) {
 
 // exersice 3.b
 int size(node *l) {
+    // Initialize count as 0
+    int count = 0;
 
-	return 0;
+    // Start from the first element of the list that isn't head
+    node *current = l->next;
+
+    // increment current while traversing list until NULL is reached
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+
+    return count;
 }
 
 // exersice 3.c and 3.d
@@ -33,6 +43,7 @@ void printout(node *l) {
   node *p = l->next;
   while (p != NULL) {
     printf("%d, ", p->data);
+    p = p->next; // answer to 3.d  // move to the next node in the list
   }
   printf("\n");
 }
@@ -42,8 +53,17 @@ int largest(node *l) {
   // pre:  head poinst to the first, empty element.
   // 	     The last element's next is NULL.
   // post: Returns the largest value of the list
+  node *p = l->next;
+    int max_value = p; // initially assume that the first element is the largest
 
-  return 0;
+   while (p != NULL) { 
+      p = p->next ;
+        if (p > max_value){
+        max_value=p ; // update max_value if a larger element is found
+
+    }
+  }
+  return (max_value);
 }
 
 #ifndef TEST
--- max.c
+++ max.c
@@ -4,15 +4,15 @@
 
 int max(int *numbers, int size) {
 
-/*pre: Pointer numbers points to an array with "size" number of elements*/
-/*post: Returns the largest numbers in the given array*/
+// pre: Pointer numbers points to an array with "size" number of elements
+// post: Returns the largest number in the given array
 
-  assert(size>0) ; /*Ensures that the array is not empty*/
-    int max_value = numbers[0]; /*initially assume that the first element is the largest*/
+  assert(size>0) ; // Ensures that the array is not empty
+    int max_value = numbers[0]; // initially assume that the first element is the largest
 
     for (int i = 1; i < size; i++) {
         if (numbers[i] > max_value) {
-            max_value = numbers[i]; /*Update max_value if a larger number is found*/
+            max_value = numbers[i]; // Update max_value if a larger number is found
         }
     }
 
